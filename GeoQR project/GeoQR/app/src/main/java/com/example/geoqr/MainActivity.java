4addeb15e534a136df7cdd54a1789c08db3adfe7
btxcy <92277569+btxcy@users.noreply.github.com>
2022-02-04 20:02:52 -0700

    Test
    Test to see if I created the branch successfully.

--- GeoQR project/GeoQR/app/src/main/java/com/example/geoqr/MainActivity.java
+++ GeoQR project/GeoQR/app/src/main/java/com/example/geoqr/MainActivity.java
@@ -10,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        System.out.println("This is a test to see if I did something wrong.");
     }
 }
