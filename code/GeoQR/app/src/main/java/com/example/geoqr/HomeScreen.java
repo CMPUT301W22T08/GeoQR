5f6615377dd584305459ad4eb39edccd16dbd7d0
mattsheydwasser <sheydwas@ualberta.ca>
2022-03-07 16:47:07 -0700

    recreated profile page files for new repo

--- code/GeoQR/app/src/main/java/com/example/geoqr/HomeScreen.java
+++ code/GeoQR/app/src/main/java/com/example/geoqr/HomeScreen.java
@@ -2,16 +2,5 @@ package com.example.geoqr;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class HomeScreen extends AppCompatActivity {
-
-    private int i;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.home_screen);
-    }
-
 }
