0e89b89fd5e273f8470049689f3a815f465a11a5
btxcy <92277569+btxcy@users.noreply.github.com>
2022-03-13 00:28:32 -0700

    optimize import statement

--- code/GeoQR/app/src/main/java/com/example/geoqr/DatabaseQR.java
+++ code/GeoQR/app/src/main/java/com/example/geoqr/DatabaseQR.java
@@ -1,17 +1,14 @@
 package com.example.geoqr;
 
-import static androidx.constraintlayout.core.motion.MotionPaths.TAG;
-
 import android.os.Bundle;
-import android.util.Log;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.firestore.*;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.ArrayList;
 
