725bc1388966ed246d223b4757c7a6338845b306
Nuo0101 <qu8@ualberta.ca>
2022-03-14 02:19:40 -0600

    test clear

--- code/GeoQR/app/src/test/java/com/example/geoqr/Test_calcScore.java
+++ code/GeoQR/app/src/test/java/com/example/geoqr/Test_calcScore.java
@@ -1,31 +1,16 @@
 package com.example.geoqr;
 
 import org.junit.*;
-//import org.junit.jupiter.api.Test;
-
+import org.junit.jupiter.api.Test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import com.google.firebase.crashlytics.buildtools.reloc.org.apache.commons.io.IOUtils;
-
-//import org.junit.jupiter.api.Test;
-
 import java.io.IOException;
 import java.net.URL;
 
 public class Test_calcScore {
     private CalculateScore test(){
-//        String url = "https://chart.googleapis.com/chart?cht=qr&chl=BFG5DGW54&chs=180x180&choe=UTF-8&chld=L|2";
-//        byte[] fileContent = new byte[0];
-//        try {
-//            fileContent = IOUtils.toByteArray(new URL(url));
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-////
-////        PowerMockito.mockStatic(testScore.class);
-
         CalculateScore c = new CalculateScore("BFG5DGW54");
         return c;
     }
