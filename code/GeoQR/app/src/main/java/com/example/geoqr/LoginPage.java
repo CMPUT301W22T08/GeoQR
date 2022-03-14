2fcdf0b300776adfbccce2643310bc444f846f8e
Chris Wu <yiming19@ualberta.ca>
2022-03-14 11:00:00 -0600

    revised version of initializing collection

--- code/GeoQR/app/src/main/java/com/example/geoqr/LoginPage.java
+++ code/GeoQR/app/src/main/java/com/example/geoqr/LoginPage.java
@@ -8,12 +8,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.geoqr.RandomString;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentChange;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
@@ -30,6 +32,7 @@ public class LoginPage extends AppCompatActivity {
     private Button btnLogin, btnGenerate;
     private EditText etUsername;
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private CollectionReference ref = db.collection("Users");
 
 
     @Override
@@ -49,28 +52,13 @@ public class LoginPage extends AppCompatActivity {
             @Override
             public void onClick(View view){
                 String username = etUsername.getText().toString();
-                String id = "";
-                String contact = "";
-                if (!TextUtils.isEmpty(username)) {  // if the username is not empty
-                    Map<String, Object> user = new HashMap<>();
-                    user.put("Username",username);
-                    user.put("ID", id);
-                    user.put("Contact", contact);
 
-                    db.collection("Users")
-                            .add(user)
-                            .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-                                @Override
-                                public void onSuccess(DocumentReference documentReference) {
-                                    Log.d(TAG, "Document added:" + documentReference.getId());
-                                }
-                            })
-                            .addOnFailureListener(new OnFailureListener() {
-                                @Override
-                                public void onFailure(@NonNull Exception e) {
-                                    Log.w(TAG,"Error adding Document", e);
-                                }
-                            });
+                QRsubcollection note = new QRsubcollection(comment, content, location, score, time);
+
+                if (!TextUtils.isEmpty(username)) {  // if the username is not empty
+                    ref.document(username)
+                            .collection("QR codes").add(note);
+                    Toast.makeText(LoginPage.this, "Success",Toast.LENGTH_LONG).show();
 
                 } else {
                     Toast.makeText(LoginPage.this, "Username cannot be empty, please re-enter!", Toast.LENGTH_SHORT).show();
