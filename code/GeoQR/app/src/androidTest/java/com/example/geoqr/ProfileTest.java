6f821a474a18521b9ae652540e3715bf136514fd
mattsheydwasser <sheydwas@ualberta.ca>
2022-03-30 15:14:00 -0600

    added more test methods

--- code/GeoQR/app/src/androidTest/java/com/example/geoqr/ProfileTest.java
+++ code/GeoQR/app/src/androidTest/java/com/example/geoqr/ProfileTest.java
@@ -1,5 +1,7 @@
 package com.example.geoqr;
 
+import android.widget.EditText;
+
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.rule.ActivityTestRule;
@@ -26,6 +28,10 @@ public class ProfileTest {
         solo = new Solo(InstrumentationRegistry.getInstrumentation(), rule.getActivity());
     }
 
+    /**
+     * checks if in correct activity
+     * @throws Exception
+     */
     @Test
     public void correctActivity() throws Exception {
 
@@ -38,9 +44,20 @@ public class ProfileTest {
      */
     @Test
     public void getDetails() throws Exception {
+
+        // click item from list in profile page, check if moves to details
         solo.clickInList(0);
         solo.assertCurrentActivity("Wrong activity", ProfileDetails.class);
 
+        // click on edit comment, edit comment, click O button
+        solo.clickOnButton("Edit Comment");
+        solo.enterText((EditText) solo.getView(R.id.detail_edit_bar), "Test String");
+        solo.clickOnButton("O");
+
+
+        // presses back arrow, checks if back in profile pag
+        solo.clickOnView(solo.getView(R.id.detail_back));
+        solo.assertCurrentActivity("Wrong activity", ProfilePage.class);
     }
 
 
